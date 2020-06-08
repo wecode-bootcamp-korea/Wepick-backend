38588ad5761ced165af5586fa06e5ebad0f8f10b
Seungho Lee <seungholee@Seunghoui-MacBook-Pro.local>
2020-06-08 21:41:29 +0900

    백엔드 초기세팅

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+
+       'default' : {
+             'ENGINE': 'django.db.backends.mysql',
+             'NAME': 'wepick',
+             'USER': 'root',
+             'PASSWORD': '50rh1754',
+             'HOST': 'localhost',
+             'PORT': '3306',
+     }
+}
+
+
