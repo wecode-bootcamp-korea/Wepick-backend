90c3edd920b340905776b99564fcd7d990c0cfb4
NHK <aptx4869.nhk@gmail.com>
2020-06-17 22:01:06 +0900

    [ADD] Sign up, Sign in, social login, profile, login required 기능 구현 및 test 완료

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path, include
-
+from django.urls import path
+from .views      import (
+    EmailCheckView, 
+    SignInView, 
+    SignUpView, 
+    SocialLoginView, 
+    JobCategoryView, 
+    CareerView, 
+    ProfileView
+)
 urlpatterns = [
+    path('/emailcheck', EmailCheckView.as_view()),
+    path('/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view()),
+    path('/sociallogin', SocialLoginView.as_view()),
+    path('/jobcategory', JobCategoryView.as_view()),
+    path('/career', CareerView.as_view()),
+    path('/profile', ProfileView.as_view()),
 ]
--- wepick_backend/urls.py
+++ wepick_backend/urls.py
@@ -1,9 +1,8 @@
-
 from django.urls import path, include
 
 urlpatterns = [
     path('account', include('account.urls')),
-    path('resume', include('resume.urls')),
-    path('company', include('company.urls')),
     path('job', include('job.urls')),
-]
+    path('company', include('company.urls')),
+    path('resume', include('resume.urls')),
+    ]
